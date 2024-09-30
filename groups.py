e166479394e03406dabe8478a3daaa82a3ad5174
cravetzm <66500001+cravetzm@users.noreply.github.com>
2024-09-30 16:27:12 -0700

    Add files via upload

---
+++ groups.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 16:20:32 2024
+
+@author: mcrav
+"""
+
+class GM_Group:
+    
+    def __init__(self, operation, inverse_operation):
+        self.operation = operation
+        self.inverse = inverse_operation
+    
+class GM_Element    
