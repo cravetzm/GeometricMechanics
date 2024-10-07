08a16f23ffb5c41c5a25936b31b62a8991b85378
cravetzm <66500001+cravetzm@users.noreply.github.com>
2024-10-06 19:09:01 -0700

    Update groups.py

--- groups.py
+++ groups.py
@@ -163,11 +163,15 @@ def draw_transform_2d(ax, T):
     return
 
 def test_group(group, elmt1, elmt2):
+    print("group results:")
     elmt3 = elmt1 * elmt2
+    print("composition of {} with {}:".format(elmt1.value,elmt2.value))
     print(elmt3.value)
     elmt1_restored = group.de_compose(elmt3,elmt2)
+    print("left inverse of {}  on {}:".format(elmt3.value,elmt3.value))
     print(elmt1_restored.value)
     elmt2_restored = group.de_compose(elmt3,elmt1)
+    print("left inverse of {}  on {}:".format(elmt3.value,elmt1.value))
     print(elmt2_restored.value)
     
     
@@ -185,20 +189,52 @@ h2 = se2.element([1,2,np.pi/2])
 test_group(se2, h1, h2)
 test_group(se2, h2, h1)
 
-#SE2 
+#SE2 plots
 fig = plt.figure()
+fig.suptitle('g, h, and their compositions')
 ax1 = fig.add_subplot(121)
+ax1.axis('equal')
 draw_transform_2d(ax1, transform_from_coordinates([0,0,0]))
 draw_transform_2d(ax1, transform_from_coordinates(h1.value))
 draw_transform_2d(ax1, transform_from_coordinates(h2.value))
-#ax.quiver([0,0], [0,0], [h1.value[0],h2.value[0]], [h1.value[1],h2.value[1]], color = salmon, scale=1)
-#ax.arrow(0, 0, h2.value[0], h2.value[1], length_includes_head = True, head_width = 0.1, color = salmon)
+ax1.annotate("origin", (0.1,0.1))
+ax1.annotate("g", (0.1,1.1))
+ax1.annotate("h", (0.9,1.9))
 ax2 = fig.add_subplot(122)
+ax2.axis('equal')
 draw_transform_2d(ax2, transform_from_coordinates([0,0,0]))
 gh = h1 * h2
 hg = h2 * h1
 draw_transform_2d(ax2, transform_from_coordinates(gh.value))
 draw_transform_2d(ax2, transform_from_coordinates(hg.value))
+ax2.annotate("origin", (0.1,0.1))
+ax2.annotate("g * h", (-0.6,2.9))
+ax2.annotate("h * g", (0.1,2.1))
+
+fig2 = plt.figure()
+#yeah it's cheesy
+g_wrt_h = np.array(h1.value) - np.array(h2.value)
+h_wrt_g = np.array(h2.value) - np.array(h1.value)
+j1 = se2.element(g_wrt_h.tolist())
+j2 = se2.element(h_wrt_g.tolist())
+#moving on
+fig2.suptitle('g, h, and their relative positions')
+ax3 = fig2.add_subplot(121)
+ax3.axis('equal')
+draw_transform_2d(ax3, transform_from_coordinates([0,0,0]))
+draw_transform_2d(ax3, transform_from_coordinates(h1.value))
+draw_transform_2d(ax3, transform_from_coordinates(h2.value))
+ax3.annotate("origin", (0.1,0.1))
+ax3.annotate("g", (0.1,1.1))
+ax3.annotate("h", (0.9,1.9))
+ax4 = fig2.add_subplot(122)
+ax4.axis('equal')
+draw_transform_2d(ax4, transform_from_coordinates([0,0,0]))
+draw_transform_2d(ax4, transform_from_coordinates(j1.value))
+draw_transform_2d(ax4, transform_from_coordinates(j2.value))
+ax4.annotate("origin", (0.1,0.1))
+ax4.annotate("g wrt h", (-0.9,-0.9))
+ax4.annotate("h wrt g", (0.9,0.9))
 
 # %% Part 2
 
@@ -270,22 +306,74 @@ class RepProductScaleShift(GM_RepGroup):
         val[1] = mat[1,2]
         
         return val
+
+class RepSE2(GM_RepGroup):
     
-    
+    def __init__(self):
         
+        super().__init__(self.rep, self.de_rep, np.identity(3))
+        
+    def rep(self, value):
+        
+        mat = transform_from_coordinates(value)
+        
+        return mat
+    
+    def de_rep(self, mat):
+        
+        x = mat[0,2]
+        y = mat[1,2]
+        theta = np.arctan2(mat[1,0], mat[1,1])
+       
+        val = [x,y,theta]
+        
+        return val
     
 def test_repgroup(group, elmt1, elmt2):
     elmt3 = elmt1 * elmt2
-    print(elmt3.value)
-    print(group.inverse(elmt3).value)
+    print("composition of {} with {}:".format(group.de_rep(elmt1.value), group.de_rep(elmt2.value)))
+    print(group.de_rep(elmt3.value))
+    print("inverse of this composition:")
+    print(group.de_rep(group.inverse(elmt3).value))
     elmt1_restored =  group.inverse(elmt2) * elmt3
-    print(elmt1_restored.value)
+    print("inverse of {} composed with {}:".format(group.de_rep(elmt2.value), group.de_rep(elmt3.value)))
+    print(group.de_rep(elmt1_restored.value))
     elmt2_restored = group.inverse(elmt1)*elmt3
-    print(elmt2_restored.value)
+    print("inverse of {} composed with {}:".format(group.de_rep(elmt1.value), group.de_rep(elmt3.value)))
+    print(group.de_rep(elmt2_restored.value))
     elmt3_adj = elmt1.AD(elmt2)*elmt1
-    print(elmt3_adj.value)
+    print("composition of {} with {} using adjoint:".format(group.de_rep(elmt1.value), group.de_rep(elmt2.value)))
+    print(group.de_rep(elmt3_adj.value))
     
 pss_rep = RepProductScaleShift()
 g1 = pss_rep.element([3, -1], from_params=True)
 g2 = pss_rep.element([0.5, 1.5], from_params=True)
 test_repgroup(pss_rep, g1, g2)
+
+se2_rep = RepSE2()
+h1 = se2_rep.element([0,1,np.pi/4], from_params = True)
+h2 = se2_rep.element([1,2,np.pi/2], from_params = True)
+test_repgroup(se2_rep, h1, h2)
+
+# same SE2 plots, but no need to make transforms
+fig = plt.figure()
+fig.suptitle('g, h, and their compositions')
+ax1 = fig.add_subplot(121)
+ax1.axis('equal')
+draw_transform_2d(ax1, transform_from_coordinates([0,0,0]))
+draw_transform_2d(ax1, h1.value)
+draw_transform_2d(ax1, h2.value)
+ax1.annotate("origin", (0.1,0.1))
+ax1.annotate("g", (0.1,1.1))
+ax1.annotate("h", (0.9,1.9))
+ax2 = fig.add_subplot(122)
+ax2.axis('equal')
+draw_transform_2d(ax2, transform_from_coordinates([0,0,0]))
+gh = h1 * h2
+hg = h2 * h1
+draw_transform_2d(ax2, gh.value)
+draw_transform_2d(ax2, hg.value)
+ax2.annotate("origin", (0.1,0.1))
+ax2.annotate("g * h", (-0.6,2.9))
+ax2.annotate("h * g", (0.1,2.1))
+
